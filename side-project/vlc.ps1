f9b5cfd092d9ff7eba008856e1bc9b9d4a39eba8
currone <94299594+snackmasta@users.noreply.github.com>
2024-01-28 23:56:47 +0700

    path

---
+++ side-project/vlc.ps1
@@ -0,0 +1,6 @@
+# Specify the paths to VLC and the video file
+$vlcPath = "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
+$videoPath = "C:\Users\Legion\Videosxu-18-720p-Btsite.mkv"
+
+# Start VLC with the specified video in fullscreen mode
+Start-Process -FilePath $vlcPath -ArgumentList "--fullscreen", $videoPath
